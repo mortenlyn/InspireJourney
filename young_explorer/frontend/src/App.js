c8c585761e0f506a253cb574e8c4ebb6a4d6baf4
Kari Johanne Sørensen Bøckman <kjbockma@stud.ntnu.no>
2024-02-07 11:47:53 +0100

    Begynt å lage destinasjons-boks for info om destninasjoner på hovedside

--- young_explorer/frontend/src/App.js
+++ young_explorer/frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Destination_box from "./components/Destination_box";
 
 function App() {
   return (
     <div className="App">
-      <Header /> MY APP
+      <Header /> MY APP 
+      <Destination_box />
     </div>
   );
 }
---
+++ young_explorer/frontend/src/components/Destination_box.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Destination_box = () => {
+  return (
+    <div className="Destination">
+      <h1 className="Destination_name">Paris</h1>
+      <div>
+        <p>
+            Paris er en europeisk verdensby som er hovedstaden i Frankrike og den mest folkerike byen i landet. Dens administrative areal er 105 km2 og den har en offisiell befolkning på 2 206 488 innbyggere.
+        </p>
+      </div>
+      <div>
+        <img src="https://lh3.googleusercontent.com/p/AF1QipM_ApMgFfAP8CP2ZHJUOb13K7P_SqSkW9sh9MFY=s1360-w1360-h1020" id="paris"></img>
+      </div>
+      <div id="ekstra_info">
+        <table>
+            <tr>
+                <span>20 000 kr</span>
+                <span>3-5 dager</span>
+                <span>19-25 grader</span>
+            </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Destination_box;
