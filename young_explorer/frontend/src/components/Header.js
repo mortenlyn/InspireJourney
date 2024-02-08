f99e781f27d06693d3576619ac8bc9947b55e98a
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-02-08 11:26:08 +0100

    Fiksa navbar og login knapp på header

--- young_explorer/frontend/src/components/Header.js
+++ young_explorer/frontend/src/components/Header.js
@@ -1,10 +1,41 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header Anette</h1>
-    </div>
+    <AppBar position="static">
+            <Toolbar>
+                {/*Inside the IconButton, we 
+                    can render various icons*/}
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    {/*This is a simple Menu 
+                      Icon wrapped in Icon */}
+                    <MenuIcon />
+                </IconButton>
+                {/* The Typography component applies 
+                     default font weights and sizes */}
+ 
+                <Typography 
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1 }}
+                >
+                    Young Explorer 
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
   );
 };
 
