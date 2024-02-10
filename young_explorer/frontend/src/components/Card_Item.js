671d2593351d1d7ac5f6905d6f346271973323e0
Kari Johanne Sørensen Bøckman <kjbockma@stud.ntnu.no>
2024-02-11 00:48:53 +0100

    da fungerer boksene! Må fikse opp litt i størrelsesorden, da de er ekstremt store, men nå kan man laste de inn og se dem!

--- young_explorer/frontend/src/components/Card_Item.js
+++ young_explorer/frontend/src/components/Card_Item.js
@@ -5,14 +5,12 @@ function CardItem(props) {
     return (
       <>
         <li className='cards__item'>
-          
             <figure data-category={props.label} className='cards__item__pic-wrap'>
-              <img src={`images/${props.src}`} alt="Travel destination" className='cards__item__img'></img>
+              <img src={`${props.src}`} alt="Travel destination" className='cards__item__img'></img>
             </figure>
             <div className='cards__item__info'>
               <h5 className='cards__item__text'>{props.text}</h5>
             </div>
-          
         </li>
       </>
     )
