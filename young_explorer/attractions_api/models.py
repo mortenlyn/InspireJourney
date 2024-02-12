83d1fc3dac6ae45ba7fc52f0ae75e3ead91cbe40
kiblindh <kiblindh@ntnu.no>
2024-02-12 12:52:53 +0100

    Have made a model

---
+++ young_explorer/attractions_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class WebsiteAttraction(models.Model):
+    attraction_id = models.AutoField(primary_key=True)
+    name = models.CharField()
+    description = models.TextField()
+    price = models.IntegerField()
+    rating = models.IntegerField()
+    date_created = models.DateField(auto_now_add=True)
+
+    REQUIRED_FIELDS = [name, description]
+
