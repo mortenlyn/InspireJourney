30e2344af7781d88422c15ba23fe51455dc8edf4
Kari Johanne Sørensen Bøckman <kjbockma@stud.ntnu.no>
2024-02-13 12:55:32 +0100

    Pulled main into branch to prepare for merge.

--- young_explorer/frontend/src/App.js
+++ young_explorer/frontend/src/App.js
@@ -10,11 +10,9 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
 
   return (
-    <Router>
       <div className="App">
         <Home /> 
       </div>
-    </Router>
   );
 }
 
