8559c97a26f885ebb590e413ce68546068f62cf7
kiblindh <kiblindh@ntnu.no>
2024-02-14 22:14:46 +0100

    Endret på form-component for å gjøre den finere

--- young_explorer/frontend/src/components/pages/AddAttraction.js
+++ young_explorer/frontend/src/components/pages/AddAttraction.js
@@ -1,8 +1,70 @@
 import React, { useState } from "react";
-
+import {
+  FormControl,
+  FormGroup,
+  InputLabel,
+  Input,
+  Grid,
+  TextField,
+  Button,
+  OutlinedInput,
+} from "@mui/material";
 
 export default function AddAttraction() {
-  const [attractionName, setAttractionName] = useState("");
+  const [attractionDescription, setAttractionDescription] = useState("");
+  const [attractionID, setAttractionID] = useState("");
+  const [price, setPrice] = useState(null);
+
+  const handlePriceInput = (event) => {
+    const value = event.target.value;
+    const checkIfInt = "/^d*$/".test(
+      value
+    ); /*This regular experession ensures that the input is a field*/
+    if (checkIfInt && parseInt(value) > 0) {
+      setPrice(value);
+    }
+  };
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={12} sm={6}>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <TextField label="Attraction Name" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Attraction Description"
+              multiline // Set multiline to true
+              fullWidth
+              rows={4} // Specify the number of rows (optional)
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Price" fullWidth onChange={handlePriceInput} />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }} size="large">
+            <Button variant="contained" color="primary">
+              Create Attraction
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+/*
+
+
+
+const [attractionName, setAttractionName] = useState("");
   const [attractionDescription, setAttractionDescription] = useState("");
   const [attractionID, setAttractionID] = useState("");
   const [price, setPrice] = useState(null);
@@ -38,30 +100,5 @@ export default function AddAttraction() {
             </label>
         </form>
     </div>
-  );
-}
-/*
-
-<FormGroup>
-        <FormControl>
-            <InputLabel>Enter attraction name</InputLabel>
-            <Input type="text"></Input>
-        </FormControl>
-        <FormControl>
-            <InputLabel>Enter description</InputLabel>
-            <Input type="text"></Input>
-        </FormControl>
-        <FormControl>
-            <InputLabel>Enter price</InputLabel>
-            <Input type="number"></Input>
-        </FormControl>
-        <FormControl>
-            <InputLabel>Enter rating</InputLabel>
-            <InputBase ></InputBase>
-        </FormControl>
-
-      </FormGroup>
-
-
-
+    );
 */
