a1da897009d891498fbc37f7023c512423b6043f
Morten Madsen Lyngstad <morteml@stud.ntnu.no>
2024-02-14 18:33:06 +0100

    Added header to login/register screen
    Also fixed so that the header fits all screen-sizes
    
    Issue: #8

--- young_explorer/frontend/src/components/LoginForm.js
+++ young_explorer/frontend/src/components/LoginForm.js
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useNavigate, Route, Routes, Link } from "react-router-dom";
 import RegisterForm from "./RegisterForm";
+import Header from "./Header";
 
 function LoginForm({ client, setIsAuthenticated }) {
   const [email, setEmail] = useState("");
@@ -23,47 +24,49 @@ function LoginForm({ client, setIsAuthenticated }) {
   }
 
   return (
-    <div className="form-container">
-      <h1 className="title">Log in</h1>
-      <form onSubmit={handleSubmit}>
-        <Form.Group className="login-register-form" controlId="emailForm">
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input-field"
-          />
-        </Form.Group>
-        <Form.Group className="login-register-form" controlId="passwordForm">
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="input-field"
-          />
-        </Form.Group>
-        <Button id="submit-btn" variant="primary" type="submit">
-          Log in
-        </Button>
-      </form>
-      <span>
-        Don't have an account?
-        <Link to="/register">Register here!</Link>
-      </span>
-      <Routes>
-        <Route
-          path="/register"
-          element={
-            <RegisterForm
-              client={client}
-              setIsAuthenticated={setIsAuthenticated}
+    <>
+      <Header />
+      <div className="form-container">
+        <h1 className="title">Log in</h1>
+        <form onSubmit={handleSubmit}>
+          <Form.Group className="login-register-form" controlId="emailForm">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-field"
             />
-          }
-        />
-      </Routes>
-    </div>
+          </Form.Group>
+          <Form.Group className="login-register-form" controlId="passwordForm">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-field"
+            />
+          </Form.Group>
+          <Button id="submit-btn" variant="primary" type="submit">
+            Log in
+          </Button>
+        </form>
+        <span>
+          Don't have an account? <Link to="/register">Register here!</Link>
+        </span>
+        <Routes>
+          <Route
+            path="/register"
+            element={
+              <RegisterForm
+                client={client}
+                setIsAuthenticated={setIsAuthenticated}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- young_explorer/frontend/src/components/RegisterForm.js
+++ young_explorer/frontend/src/components/RegisterForm.js
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useNavigate, Route, Routes, Link } from "react-router-dom";
 import LoginForm from "./LoginForm";
+import Header from "./Header";
 
 function RegisterForm({ client, setIsAuthenticated }) {
   const [email, setEmail] = useState("");
@@ -31,56 +32,58 @@ function RegisterForm({ client, setIsAuthenticated }) {
   }
 
   return (
-    <div className="form-container">
-      <h1 className="title">Register</h1>
-      <form onSubmit={handleSubmit}>
-        <Form.Group className="login-register-form" controlId="emailForm">
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input-field"
-          />
-        </Form.Group>
-        <Form.Group className="login-register-form" controlId="usernameForm">
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="input-field"
-          />
-        </Form.Group>
-        <Form.Group className="login-register-form" controlId="passwordForm">
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="input-field"
-          />
-        </Form.Group>
-        <Button id="submit-btn" variant="primary" type="submit">
-          Register
-        </Button>
-      </form>
-      <span>
-        Already have an account?
-        <Link to="/login">Login here!</Link>
-      </span>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <LoginForm
-              client={client}
-              setIsAuthenticated={setIsAuthenticated}
+    <>
+      <Header />
+      <div className="form-container">
+        <h1 className="title">Register</h1>
+        <form onSubmit={handleSubmit}>
+          <Form.Group className="login-register-form" controlId="emailForm">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-field"
             />
-          }
-        />
-      </Routes>
-    </div>
+          </Form.Group>
+          <Form.Group className="login-register-form" controlId="usernameForm">
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-field"
+            />
+          </Form.Group>
+          <Form.Group className="login-register-form" controlId="passwordForm">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-field"
+            />
+          </Form.Group>
+          <Button id="submit-btn" variant="primary" type="submit">
+            Register
+          </Button>
+        </form>
+        <span>
+          Already have an account? <Link to="/login">Login here!</Link>
+        </span>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <LoginForm
+                client={client}
+                setIsAuthenticated={setIsAuthenticated}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
