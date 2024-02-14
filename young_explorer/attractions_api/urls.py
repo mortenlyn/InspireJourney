bb138fcfbd9d3cdf42d6f468f120029ce1d3e82c
Kim-Iver Brevik Blindheimsvik <kiblindh@stud.ntnu.no>
2024-02-14 17:17:44 +0100

    Update 2 files
    - /young_explorer/attractions_api/models.py
    - /young_explorer/attractions_api/urls.py

--- young_explorer/attractions_api/urls.py
+++ young_explorer/attractions_api/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 # URL patterns for the API
 urlpatterns = [
-    path('admin', admin.site.urls),
     path('attractions', views.attraction_view.as_view()),
     path('addAttraction', views.addAttraction.as_view()),
 ]
