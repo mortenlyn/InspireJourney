c358eeb58f9db7234f5767f75e52484973674c39
Ulrik Haugland <ulhaugland@gmail.com>
2024-02-19 11:22:14 +0100

    Fjernet duplikatmappen

--- young_explorer/frontend/young_explorer/young_explorer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user_api/', include('user_api.urls')),
-    path('attractions_api/', include('attractions_api.urls')),
-]
