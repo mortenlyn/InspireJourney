30c885f650f3198da79a664dfac6634a5504d02f
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-02-27 11:46:45 +0100

    Gjort headeren på forsiden større

--- young_explorer/frontend/src/components/header-section.js
+++ young_explorer/frontend/src/components/header-section.js
@@ -7,17 +7,42 @@ import './header-section.css';
 function headersection() {
     //let navigate = useNavigate();
 
-    const routeChange = () => { 
+    /*const routeChange = () => { 
         let path = '/services'; 
         //navigate(path);
-      }
+      }*/
     
       return (
         <div className='header-container'>
-            <image src='./assets/Header-bilde.jpg'></image>
-            <h1>Adventure Awaits</h1>
+          <img src='./cropped-header-bilde.jpg'
+
+            style={{ marginTop: -100 , width: 1450}}
+            id='beach-picture'
+            alt='beach and water'
+          />
+          <h1 id="header-text" style={{ position: 'absolute', top: '25%', left: '50%',  transform: 'translate(-50%, -50%)', zIndex: 1 }}>Adventure Awaits</h1>
         </div>
       )
+
+              //style={{height: '337px', objectFit: 'scale-down' }}
+      /*<div className='cropped-image-container'>
+            <img src='./cropped-header-bilde.jpg'
+              style={{marginTop:-100}}
+              id='beach-picture' alt='beach and water'/>
+          </div>*/
+
+          /*<div className='header-container'>
+          <img src='./cropped-header-bilde.jpg'
+              style={{marginTop:-100}}
+              id='beach-picture' alt='beach and water'/>
+          <h1>Adventure Awaits</h1>
+        </div>*/
+
+      
+            
+          
+
+           
 
 } 
 
