6e094b43920404045a149a6b83f407e80ca6f573
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-02-29 14:14:50 +0100

    Endret navn i database

--- young_explorer/young_explorer/settings.py
+++ young_explorer/young_explorer/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'young_explorer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
+        'NAME': 'sample',
         'USER': 'root',
         'PASSWORD': 'root1234',
         'HOST': 'localhost',
