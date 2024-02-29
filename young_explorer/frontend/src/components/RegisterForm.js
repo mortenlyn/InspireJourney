747fd56ecda6f4765ed449db7bd28ea3c6275c26
Johny <hjnguyen@ntnu.no>
2024-02-29 11:53:49 +0100

    Added feedback if password length is too short.

--- young_explorer/frontend/src/components/RegisterForm.js
+++ young_explorer/frontend/src/components/RegisterForm.js
@@ -16,6 +16,9 @@ function RegisterForm({ client, setCurrentUser }) {
     event.preventDefault();
     if (password !== confirmPassword) {
       alert("Passwords don't match");
+    } 
+    else if (password.length < 8) {
+      alert("Password must be at least 8 characters");
     }
     else{
       client
