9285bd8b48fff27bba353766c071b95b4f4d308f
kiblindh <kiblindh@ntnu.no>
2024-03-03 13:41:55 +0100

    Have implemented filtering on homescreen
    ## Changes
    1) Have changed it so destination_box includes FilterBox which has all the content necessary for destinations. This is because i want the homepage as empty as possible
    
    2) Have changed the name of filter_by_labels.js to FilterBox since i got an error about the component name. Components in React needs to start with a capital letter
    
    3) Have made filter_box call GetAllAttractions the first time, and then it does the filtering logic once we press the buttons. This can be simplified later by only using the filter_box sicne the empty endpoint of filter/? will give you all destinations

--- young_explorer/frontend/src/components/pages/Home.js
+++ young_explorer/frontend/src/components/pages/Home.js
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
 import Destination_box from "../Destination_box";
 import "../../App.css";
-import Header from "../Header";
-import Filter_box from "../filter_by_labels";
+import FilterBox from "../FilterBox";
 
 function Home(props) {
-  // Define filteredAttractions state
-  const [filteredAttractions, setFilteredAttractions] = useState([]);
-
-  // Define a function to update filteredAttractions
-  const updateFilteredAttractions = (attractions) => {
-    setFilteredAttractions(attractions);
-  };
 
   return (
     <div>
-      {/* Render Filter_box component and pass updateFilteredAttractions as a prop */}
-      <Filter_box setFilteredAttractions={updateFilteredAttractions} />
-      
-      {/* Render Destination_box component and pass filteredAttractions as a prop */}
-      <Destination_box
-        currentUser={props.currentUser}
-        filteredAttractions={filteredAttractions}
-      />
+      <Destination_box currentUser={props.currentUser}/>
     </div>
   );
 }
