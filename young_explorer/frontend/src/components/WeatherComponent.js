ffac7b01f33d20935c345cdd9639c0864104b31e
Morten M Lyngstad <Morteml@stud.ntnu.no>
2024-03-15 12:55:02 +0100

    Added weather info on destination page
    Changes:
    - Added weatherIcon svgs
    - Added logic so temp over 0 is red and under 0 is blue
    
    Co-authored-by: @olepmy
    Issue: #46

--- young_explorer/frontend/src/components/WeatherComponent.js
+++ young_explorer/frontend/src/components/WeatherComponent.js
@@ -1,51 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import weatherAPI from "../api/weatherAPI";
 import "./WeatherComponent.css";
-import rain from "../assets/rain.jpg"
+import Rain from "../assets/Rain.svg";
+import Snow from "../assets/Snow.svg";
+import Clear from "../assets/Clear.svg";
+import fewclouds from "../assets/fewclouds.svg";
+import Thunderstorm from "../assets/Thunderstorm.svg";
+import Clouds from "../assets/Clouds.svg";
+import shrug from "../assets/shrug.svg";
 
 function WeatherComponent(props) {
   const [weather, setWeather] = useState({});
-
-  const getWeather = async () => {
-    fetch(
-      `${weatherAPI.base}weather?q=${props.name}&units=metric&APPID=${weatherAPI.key}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setWeather(data);
-        console.log(weather);
-      });
+  const kindsofweather = ["Rain", "Snow", "Clear", "Thunderstorm", "Clouds"];
+  const weatherIcons = {
+    Rain: Rain,
+    Snow: Snow,
+    Clear: Clear,
+    Thunderstorm: Thunderstorm,
+    Clouds: Clouds,
   };
+
+  useEffect(() => {
+    const getWeather = async () => {
+      fetch(
+        `${weatherAPI.base}weather?q=${props.name}&units=metric&APPID=${weatherAPI.key}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data);
+          console.log(weather);
+        });
+    };
+
+    getWeather();
+  }, []);
 
   return typeof weather.main != "undefined" ? (
     <>
       <li className="weather_item">
         <div className="weather_item_box">
-          <h3>Weather in {props.name}</h3>
-          <button id="myButton" onClick={getWeather}>
-            Get Weather
-          </button>
-          <h2>Temperature: {Math.round(weather.main.temp)} °C</h2>
-          <h2>Condition: {weather.weather[0].main}</h2>
-          <h2>Humidity: {weather.main.humidity} %</h2>
-          <h2>Wind: {Math.round(weather.wind.speed)} m/s</h2>
-        
-         
-          {weather.weather[0].main == "Rain" ? (
-            <>
-              <img src={rain} alt="High Humidity" />
-              <p>Its raining ing {props.name}, you should bring and umbrella!</p>
-            </>
-          ) : null}
+          {Math.round(weather.main.temp) > 0 ? (
+            <h1 id="weatherh1">
+              Temperature:{" "}
+              <span style={{ color: "red" }}>
+                {Math.round(weather.main.temp)} °C{" "}
+              </span>
+            </h1>
+          ) : (
+            <h1 id="weatherh1">
+              Temperature:{" "}
+              <span style={{ color: "blue" }}>
+                {Math.round(weather.main.temp)} °C{" "}
+              </span>
+            </h1>
+          )}
 
-           {weather.weather[0].main == "Thunderstorm" ? (
+          <h1 id="weatherh1">Condition: {weather.weather[0].main}</h1>
+          <h1 id="weatherh1">Humidity: {weather.main.humidity} %</h1>
+          <h1 id="weatherh1">Wind: {Math.round(weather.wind.speed)} m/s</h1>
+          {weather.weather[0].description === "few clouds" ? (
+            <img src={fewclouds} alt="Weather conditions" />
+          ) : !kindsofweather.includes(weather.weather[0].main) ? (
             <>
-              <img src={rain} alt="High Humidity" />
-              <p>Its raining ing {props.name}, you should bring and umbrella!</p>
+              <img src={shrug} alt="Weather conditions" width="200" />
+              <h1 id="weatherh1">
+                Sorry, we don't have an icon for this weather
+              </h1>
             </>
-          ) : null}
-
+          ) : (
+            <img
+              src={weatherIcons[weather.weather[0].main]}
+              alt="Weather conditions"
+              width="275px"
+            />
+          )}
+          <div className="weather_item_info">
+            <h5 id="weather_text" className="weather_item_text">
+              Weather in {props.name}
+            </h5>
           </div>
+        </div>
       </li>
     </>
   ) : (
@@ -53,9 +87,6 @@ function WeatherComponent(props) {
       <li className="weather_item">
         <div className="weather_item_box">
           <h3>Weather in {props.name}</h3>
-          <button id="myButton" onClick={getWeather}>
-            Get Weather
-          </button>
         </div>
       </li>
     </>
